de6b5777cfa0a16cc82420a9f1f781fb5142f909
Shimbo Takaaki <ta7uwtaka@gmail.com>
2019-07-14 15:50:33 +0900

    Add print function for vector<vector T>>

--- pyprint.h
+++ pyprint.h
@@ -7,14 +7,16 @@ using namespace std;
 
 template<typename T>
 
-void print(const vector<T> &v) {
+void print(const vector<T> &v, bool endLine = true) {
     int len = v.size();
+    cout << "[";
     for (int i = 0; i < len; ++i) {
         cout << v[i];
         if (i != len - 1) {
             cout << ", ";
         }else{
-            cout << endl;
+            cout << "]";
+            if(endLine) cout << endl;
         }
     }
 }
@@ -22,4 +24,14 @@ void print(const vector<T> &v) {
 template<typename T1, typename T2>
 void print(const pair<T1, T2> &p) {
      cout << "(" <<  p.first << ", " << p.second << ")" << endl;
+}
+
+template<typename T> void print(const vector<vector<T>> &grid) {
+    int len = grid.size();
+    cout << "[";
+    for (int i = 0; i < len; ++i) {
+        print(grid[i], false);
+        if (i != len -1) cout << "," << endl;
+    }
+    cout << "]" << endl;
 }
