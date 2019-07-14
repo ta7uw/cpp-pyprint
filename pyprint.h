1441a8b243b03fdf34e13f097717de476795a9c5
Shimbo Takaaki <ta7uwtaka@gmail.com>
2019-07-14 15:54:07 +0900

    Fix style

--- pyprint.h
+++ pyprint.h
@@ -14,24 +14,25 @@ void print(const vector<T> &v, bool endLine = true) {
         cout << v[i];
         if (i != len - 1) {
             cout << ", ";
-        }else{
+        } else {
             cout << "]";
-            if(endLine) cout << endl;
+            if (endLine) cout << endl;
         }
     }
 }
 
 template<typename T1, typename T2>
 void print(const pair<T1, T2> &p) {
-     cout << "(" <<  p.first << ", " << p.second << ")" << endl;
+    cout << "(" << p.first << ", " << p.second << ")" << endl;
 }
 
-template<typename T> void print(const vector<vector<T>> &grid) {
+template<typename T>
+void print(const vector<vector<T>> &grid) {
     int len = grid.size();
     cout << "[";
     for (int i = 0; i < len; ++i) {
         print(grid[i], false);
-        if (i != len -1) cout << "," << endl;
+        if (i != len - 1) cout << "," << endl;
     }
     cout << "]" << endl;
 }
