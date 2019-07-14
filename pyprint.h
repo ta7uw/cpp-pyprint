b55678722f9ac717d0922294808212baabe31295
Shimbo Takaaki <ta7uwtaka@gmail.com>
2019-07-14 19:05:55 +0900

    Add print function for map

--- pyprint.h
+++ pyprint.h
@@ -2,6 +2,7 @@
 
 #include<vector>
 #include<set>
+#include<map>
 #include<iostream>
 
 using namespace std;
@@ -56,6 +57,19 @@ void print(set<T> &s) {
     cout << "(";
     for (auto it : s) {
         cout << it;
+        if (cnt != len - 1) cout << ", ";
+        cnt++;
+    }
+    cout << ")" << endl;
+}
+
+template<typename T1, typename T2>
+void print(map<T1, T2> &dict) {
+    long long len = dict.size();
+    long long cnt = 0;
+    cout << "(";
+    for (auto it = dict.begin(); it != dict.end(); ++it) {
+        cout << it->first << ": " << it->second;
         if (cnt != len - 1) cout << ", ";
         cnt++;
     }
